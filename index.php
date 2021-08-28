6727d5ae0f0f714eefeedc66a49355336aa2a8ad
Shanmugapriya-webdp <64578528+Shanmugapriya-webdp@users.noreply.github.com>
2021-08-28 23:26:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,94 +1,85 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-<br /><br />
-<div class="container" style="width:600px;">
-    <select name="city" id="city" class="form-control input-lg">
-        <option value="">Select City</option>
-    </select>
-    <br />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<center>
+<div class="container">
+  <div>
     <select name="District" id="District" class="form-control input-lg">
-        <option value="">Select District</option>
+      <option value="">Select District</option>
     </select>
-    <br />
-    <button name="Street" id="Street" class="form-control input-lg">Get StreetName
-    </button>
-    <lable>Street Name is:</lable>
+    <br /><br />
+    <select name="City" id="City" class="form-control input-lg">
+      <option value="">Select City</option>
+    </select>
+    <br /><br />
+     <input type="button" name="Street" value="Get street Name" id="Street">
+
+
+
+<!--          <select name="Street" id="Street" class="form-control input-lg">-->
+<!--      <option value="">Select street</option>-->
+<!--    </select>-->
+  </div>
 </div>
-</body>
-</html>
 
 <script>
-    $(document).ready(function(){
 
-        load_json_data('country');
+var Area = [];
+var html_code = '';
+$.getJSON('sample.json', function(data) {
+  
+   var html_code = '';
+    Area = data;
+  $.each(Area, function(key, val) {
+	html_code +=
+      '<option value="' + val.District + '">' + val.District + '</option>';
+  });
+$('#District').html(html_code);
 
-        function load_json_data(District, City)
-        {
-            var html_code = '';
-            $.getJSON('sample.json', function(data){
+});
 
-                html_code += '<option value="">Select '+City+'</option>';
-                $.each(data, function(key, value){
-                    if(City == 'country')
-                    {
-                        if(value.District == '0')
-                        {
-                            html_code += '<option value="'+value.City+'">'+value.City+'</option>';
-                        }
-
-                    }
-                    else
-                    {
-                        if(value.District == District)
-                        {
-                            html_code += '<option value="'+value.City+'">'+value.City+'</option>';
-                        }
-                    }
-                });
-                $('#'+id).html(html_code);
-            });
-
-        }
-
-        $(document).on('change', '#city', function(){
-            var country_id = $(this).val();
-            if(country_id != '')
-            {
-                load_json_data('District', country_id);
-            }
-            else
-            {
-                $('#state').html('<option value="">Select state</option>');
-                $('#city').html('<option value="">Select city</option>');
-            }
-        });
-        $(document).on('change', '#district', function(){
-            var state_id = $(this).val();
-            if(state_id != '')
-            {
-                load_json_data('City', state_id);
-            }
-            else
-            { 
-                $('#city').html('<option value="">Select city</option>');
-            }
-        });
-		    $(document).on('change', '#street', function(){
-            var city_id = $(this).val();
-            if(city_id != '')
-            {
-                load_json_data('Street', city_id);
-            }
-            else
-            {
-                $('#district').html('<option value="">Select District</option>');
-            }
-        });
+function updateDropdown(District, City, Street) {
+  if (!City) {
+    html_code = '<option value="">Select City</option>';
+    $.each(Area, function (index, value) {
+      if (value.District == District) {
+        html_code +=
+          '<option value="' + value.City + '">' + value.City + '</option>';
+      }
     });
+
+    $('#City').html(html_code);
+    $('#Street').html('<option value="">Select movies</option>');
+  } else {
+    html_code = '<option value="">Select movies</option>';
+    $.each(Area, function (index, value) {
+      if (value.District == District) {
+        html_code +=
+          '<option value="' + value.Street + '">' + value.Street + '</option>';
+      }
+    });
+    $('#Street').html(html_code);
+  }
+}
+
+$('#District, #City, #Street').on('change', function () {
+  if (this.id == 'District') {
+
+    $('#City').prop('selectedIndex', 0);
+    $('#Street').prop('selectedIndex', 0);
+  } 
+  
+  if (this.id == 'Street') {
+    console.log('The Street is: ' + $(this).val());
+  } else {
+    var District = $('#District').val();
+    var City = $('#City').val();
+    var Street = $('#Street').val();
+    updateDropdown(District, City, Street);
+  }
+});
 </script>
+    <script>
+      function myFunction()
+      {
+          document.getElementById("street").innerHTML="";
+      }
+    </script>
